d0f258c6d7255331823b7d82f00218e8f9851d4c
creativoma <ma.marianoalvarez@gmail.com>
2022-03-11 20:30:19 +0100

    Organizando componentes y optimizando código

--- scripts/componentes/scroll.js
+++ scripts/componentes/scroll.js
@@ -1,18 +1,1 @@
-export const componenteScroll = () =>{
-    let mybutton = document.getElementById("btn-top");
-
-    document.onscroll = function () {
-        scrollFunction();
-    };
-
-    function scrollFunction() {
-        if (
-            document.body.scrollTop > 50 ||
-            document.documentElement.scrollTop > 50
-        ) {
-            mybutton.classList = "active";
-        } else {
-            mybutton.classList = "inactive";
-        }
-    }
-}
+export const componenteScroll=()=>{let o=document.getElementById("btn-top");document.onscroll=function(){document.body.scrollTop>50||document.documentElement.scrollTop>50?o.classList="active":o.classList="inactive"}};
